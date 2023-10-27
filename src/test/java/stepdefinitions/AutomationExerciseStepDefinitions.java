523d35817217c3fcacabf8db979ec5e497eabd0a
ialpery <ialper.yigit@gmail.com>
2023-10-27 22:30:43 +0300

    first commit

---
+++ src/test/java/stepdefinitions/AutomationExerciseStepDefinitions.java
@@ -0,0 +1,42 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AutomationExerciseStepDefinitions {
+
+    @Given("user navigated to url")
+    public void user_navigated_to_url() {
+
+    }
+    @When("Verify that home page is visible successfully")
+    public void verify_that_home_page_is_visible_successfully() {
+
+    }
+    @When("Click cart button")
+    public void click_button() {
+
+    }
+    @When("Scroll down to footer")
+    public void scroll_down_to_footer() {
+
+    }
+    @When("Verify text SUBSCRIPTION")
+    public void verify_text() {
+
+    }
+    @When("Enter email address in input and click arrow button")
+    public void enter_email_address_in_input_and_click_arrow_button() {
+
+    }
+    @When("Verify success message You have been successfully subscribed! is visible")
+    public void verify_success_message_is_visible() {
+
+    }
+    @Then("The page will closed")
+    public void the_page_will_closed() {
+
+    }
+
+}
---
+++ src/test/resources/features/TC11_subscriptionInCartPage.feature
@@ -0,0 +1,12 @@
+Feature: Subscription in Cart Page
+
+  Scenario: User will subscribe in cart page
+
+    Given user navigated to url
+    When Verify that home page is visible successfully
+    And Click cart button
+    And Scroll down to footer
+    And Verify text SUBSCRIPTION
+    And Enter email address in input and click arrow button
+    And Verify success message You have been successfully subscribed! is visible
+    Then The page will closed
